d79d3034e0721f3c0c083f436348470439cd8372
Chaitra-Kulkarni <rkchaitra22@gmail.com>
2022-04-26 08:29:28 +0530

    first challenge - reverse a string

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 // 1. Reverse a String
 function reverseString(str) {
-    let strArr = str.split("");
-    let reverseString = strArr.reverse();
-    let reversedString = reverseString.join("");
-    return reversedString;
+// Using built-in methods
+
+    // let strArr = str.split("");
+    // let reverseString = strArr.reverse();
+    // let reversedString = reverseString.join("");
+    // return reversedString;
+
+    // return str.split("").reverse().join("");
+
+// Using for loop
+    let final = "";
+    for(let i=str.length-1; i>=0; i--) {
+        // console.log(str[i]);
+        final += str[i];
+    }
+    return final.toUpperCase();
 }
 
 console.log(reverseString("chaitra"));
